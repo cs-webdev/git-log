43e96882fdaee8a956a4f4113905a48fc6d9179f
Márcio Coelho <marcio@marcio-coelhos-macbook.local>
2014-06-25 15:12:50 +0100

    Test pass with the new information. Refactory Commit Controller

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,29 +1,13 @@
 class CommitsController < ApplicationController
   before_filter :get_repo
-  attr_accessor :sha, :message, :author_name, :parent_sha, :parents_size
 
-  def initialize(message, attributes = {})
-    @sha=attributes[:sha]
-    @message=message
-    @author_name=attributes[:author_name]
-    @parent_sha=attributes[:parent_sha]
-    @parents_size=attributes[:parents_size]
-  end
-
-	def show
-    repo_commit = @repo.commit(params[:id])
-    @commit  = Commit.new(repo_commit.message,
-      sha: repo_commit.sha,
-      author_name: repo_commit.author.name,
-      parent_sha: repo_commit.parent.sha,
-      parents_size: repo_commit.parents.size
-    )
-    @other_commits = @repo.other_commits(@commit.sha)
-    @stats = repo_commit.diff_parent.stats if( @commit.parents_size > 0)
-	end
-
-  private
   def get_repo
     @repo = Repository.find(params[:repository_id])
   end
+
+	def show
+    @commit = @repo.commit(params[:id])
+    @other_commits = @repo.other_commits(@commit.sha)
+    @stats = @commit.diff_parent.stats if( @commit.parents.size > 0)
+	end
 end
--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,8 +1,8 @@
 class Commit
-	attr_reader :sha,:message
+	attr_reader :sha, :message
 
-	def self.commit_details( sha )
-		repo = Git.open(working_dir, :log => Logger.new(STDOUT))
-		repo.gcommit(sha)
-	end
+  def self.commit_details(sha)
+    repo = Git.open(working_dir, :log => Logger.new(STDOUT))
+    repo.gcommit(sha)
+  end
 end
