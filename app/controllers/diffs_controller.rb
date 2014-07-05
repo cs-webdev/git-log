d9b533a998aa5f57e60d9cf7afb0187a4e5bfe13
Raphael Rodrigues <raphael@mbp-de-raphael.home>
2014-07-05 12:28:28 +0100

    Refactor-Travis

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -1,6 +1,6 @@
-class DiffController < ApplicationController
+class DiffsController < ApplicationController
   expose(:repository)
-  
+
   def show
     @sha  = params[:commit_id]
     @diff = repository.diff_commits(params[:commit_id], params[:id])
